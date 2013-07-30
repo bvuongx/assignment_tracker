4068ba60dfd9fd1d76cbc015062b0285887c21bd
bvuongx <bvuong@gmail.com>
2013-07-30 16:45:08 -0700

    added some green buttons and a seed file for students and projects

--- db/migrate/20130729003805_create_assignments.rb
+++ db/migrate/20130729003805_create_assignments.rb
@@ -1,8 +1,8 @@
 class CreateAssignments < ActiveRecord::Migration
   def change
     create_table :assignments do |t|
-      t.belongs_to :project
-      t.belongs_to :student
+      t.references :project
+      t.references :student
       t.string     :grade
       t.date 	   :turned_in
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+students = Student.create([{ first_name: 'Bill' }, { first_name: 'Jane' }, { first_name: 'Brian' }, { first_name: 'Tom' }, { first_name: 'Ellen' }, { first_name: 'Candace' }]
+students = Student.create([{ last_name: 'Smith' }, { last_name: 'White' }, { last_name: 'Lee' }, { last_name: 'Black' }, { last_name: 'Potter' }, { last_name: 'Yogi' }]
+projects = Project.create([{ title: 'Math Homework' }, { title: 'A new quilt' }, { title: 'Diorama for History' }, { title: 'Science Fair project' }, { title: 'Art Project' }, { title: 'Chemistry Experiment' }]
